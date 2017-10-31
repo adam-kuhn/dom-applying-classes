8ee9d16ddf7771a37a957a973dc986480d1d74fa
adam-kuhn <adam.kuhn20@gmail.com>
2017-10-31 09:47:15 +0200

    make dotted box visible

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
 two();
 three ();
+makeVisible ();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -31,3 +32,8 @@ function three () {
   three.classList.add('orange');
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible');
+  makeVisible[0].classList.add('visible');
+
+}
